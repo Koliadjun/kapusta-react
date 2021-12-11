3fa3ff708a4a8aa826fdccdbc4c7b0f44c2ed7e2
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-12-11 22:29:05 +0200

    add_Summary

---
+++ src/components/Summary/Summary.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Summary({items}) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Сводка</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+       </table>
+    )
+}
