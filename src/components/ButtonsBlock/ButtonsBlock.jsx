9c732c82e02172b0fcd99af9f8b1e6884b3fb6b8
Dima <75967292+DukaDima@users.noreply.github.com>
2021-12-12 15:38:59 +0200

    Button and buttons components

---
+++ src/components/ButtonsBlock/ButtonsBlock.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import Button from 'components/Button/Button';
+import s from './ButtonsBlock.module.css';
+
+export default function ButtonsBlock({
+  textLeftButton = 'ввод',
+  textRightButton = 'очистить',
+  onClickLeftButton,
+  onClickRightButton,
+  rectangle = false,
+}) {
+  return (
+    <div className={rectangle ? s.rectandleButtons : s.buttons}>
+      <Button
+        text={textLeftButton}
+        onClick={onClickLeftButton}
+        rectangle={rectangle}
+      />
+      <Button
+        text={textRightButton}
+        onClick={onClickRightButton}
+        rectangle={rectangle}
+      />
+    </div>
+  );
+}
+
+ButtonsBlock.propTypes = {
+  textLeftButton: propTypes.string,
+  textRightButton: propTypes.string,
+  onClickLeftButton: propTypes.func.isRequired,
+  onClickRightButton: propTypes.func.isRequired,
+  rectangle: propTypes.bool,
+};
