70c3dc9c9c2aa00f55a2076a604163b0ac748dbe
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-12-12 13:23:53 +0200

    add_Summary

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import s from './Summary.module.css'
+import items from './items.json'
 
-export default function Summary({items}) {
+export default function Summary() {
+    
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Сводка</th>
-                </tr>
-            </thead>
-            <tbody>
-                
-            </tbody>
-       </table>
-    )
+    <div className={s.wrap}>
+      <table className={s.reportHistory}>
+        <thead>
+          <tr className={s.tr}>
+            <th className={s.reportHeader} colSpan="3">
+              Сводка
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(x => (
+            <tr className={s.tr} key={x.month}>
+              <td className={s.month}>{x.month}</td>
+              <td className={s.sum}>{x.sum}</td>
+            </tr>
+          ))}
+          <tr className={s.empty}>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
