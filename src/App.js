83761c13758e8ba2d634d882274f53d6aacacec8
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-12-13 09:40:49 +0200

    add AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
+import AppBar from 'components/AppBar/AppBar';
 
 function App() {
   return (
     <div>
+      <AppBar />
       <ButtonsBlock />
     </div>
   );
