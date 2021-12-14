c86380037aa3c7d2efad3e28ea411a27084d4126
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-15 00:13:13 +0200

    disable midlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,23 +2,23 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import {
     persistStore,
     persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+    // FLUSH,
+    // REHYDRATE,
+    // PAUSE,
+    // PERSIST,
+    // PURGE,
+    // REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 // import transactionReducer from 'redux/transaction/transaction-reducer';
 import authReducer from 'redux/authorization/authorization-slice';
-const middleware = [
-    ...getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
-];
+// const middleware = [
+//     ...getDefaultMiddleware({
+//         serializableCheck: {
+//             ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//         },
+//     }),
+// ];
 const authPersistConfig = {
     key: 'auth',
     storage,
@@ -30,7 +30,7 @@ export const store = configureStore({
         // transaction: transactionReducer,
         auth: persistReducer(authPersistConfig, authReducer),
     },
-    middleware,
-    devTools: process.env.NODE_ENV !== 'production',
+    // middleware,
+    // devTools: process.env.NODE_ENV !== 'production',
 });
 export const persistor = persistStore(store);
