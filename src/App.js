1caaabfe10192216f686610f78dd0a74bf0c2fcf
Dima <75967292+DukaDima@users.noreply.github.com>
2021-12-14 19:27:19 +0200

    Модалка универсальный контент модалки

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 
 import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
+import Modal from 'components/Modal/Modal';
+import ModalContent from 'components/ModalContent/ModalContent';
 
 function App() {
+  const [modalActive, setModalActive] = useState(false);
   return (
     <div>
       <ButtonsBlock />
+      <Modal active={modalActive} setActive={setModalActive}>
+        <ModalContent
+          message={'Вы уверены?'}
+          textLeftButton={'да'}
+          textRightButton={'нет'}
+        />
+      </Modal>
+
+      <button onClick={() => setModalActive(true)}>Проверка модалки</button>
     </div>
   );
 }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import s from './Modal.module.css';
+
+export default function Modal({ active, setActive, children }) {
+  return (
+    <div
+      className={active ? `${s.modal} ${s.active}` : s.modal}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={
+          active ? `${s.modal__content} ${s.active}` : s.modal__content
+        }
+        onClick={e => e.stopPropagation()}
+      >
+        <button className={s.modalButton} onClick={() => setActive(false)}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
