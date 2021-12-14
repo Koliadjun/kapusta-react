9ad50798a1713c467343ab7b8319e0c4109b08a9
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-12-14 10:35:34 +0200

    Update CategoryList.jsx

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,27 +1,32 @@
 import React, { useState } from 'react';
 import list from './list.json';
+import s from './styles.module.css';
 
 export default function CategoryList() {
-    const[category,setCategory]=useState('')
+  const [category, setCategory] = useState('');
 
   const handlerChange = event => {
     event.preventDefault();
-    setCategory(event.target.value)
-   };
+    setCategory(event.target.value);
+  };
 
   return (
     <form>
-      <input
-        list="category"
-        name="categotyItem"
-        placeholder="Категория товара"
-        onChange={handlerChange}
-      />
-
-      <datalist id="category">
+      <span className={s.inputWrapper}>
+        <input
+          type="text"
+          className={s.categoryInput}
+          list="category"
+          name="categotyItem"
+          placeholder="Категория товара"
+          onChange={handlerChange}
+          />
+        </span>
+      
+      <datalist id="category" className={s.datalist}>
         {list.map(item => {
           return (
-            <option key={item.id} value={item.name}>
+            <option key={item.id} value={item.name} className={s.datalistItem}>
               {item.name}
             </option>
           );
