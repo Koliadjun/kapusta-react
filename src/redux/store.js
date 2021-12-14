51e2a6b0643e439ed5ae3431d19f1ddee7893ff6
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-15 00:04:40 +0200

    initiate store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,36 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import transactionReducer from 'redux/transaction/transaction-reducer';
+import authReducer from 'redux/authorization/authorization-slice';
+const middleware = [
+    ...getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+];
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+};
 
+export const store = configureStore({
+    reducer: {
+        // transaction: transactionReducer,
+        auth: persistReducer(authPersistConfig, authReducer),
+    },
+    middleware,
+    devTools: process.env.NODE_ENV !== 'production',
+});
+export const persistor = persistStore(store);
