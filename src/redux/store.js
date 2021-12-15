bb731455d79b5180a7017987fb6cea4f26c44e8a
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-16 00:50:31 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import {
     persistStore,
     persistReducer,
-    // FLUSH,
-    // REHYDRATE,
-    // PAUSE,
-    // PERSIST,
-    // PURGE,
-    // REGISTER,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import transactionReducer from 'redux/transaction/transaction-reducer';
+
 import authReducer from 'redux/authorization/authorization-slice';
-// const middleware = [
-//     ...getDefaultMiddleware({
-//         serializableCheck: {
-//             ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//         },
-//     }),
-// ];
+import transactionReducer from 'redux/transaction/transaction-slice'
+const middleware = [
+    ...getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+];
 const authPersistConfig = {
     key: 'auth',
     storage,
@@ -27,10 +28,10 @@ const authPersistConfig = {
 
 export const store = configureStore({
     reducer: {
-        // transaction: transactionReducer,
+        transaction: transactionReducer,
         auth: persistReducer(authPersistConfig, authReducer),
     },
-    // middleware,
-    // devTools: process.env.NODE_ENV !== 'production',
+    middleware,
+    devTools: process.env.NODE_ENV !== 'production',
 });
 export const persistor = persistStore(store);
