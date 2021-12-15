d20a68277b5bc08cebb049e9093e367031b1e9dd
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-12-15 09:21:57 +0200

    test isLogin

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -8,14 +8,18 @@ import s from './AppBar.module.css'
 
 
 export default function AppBar() {
-const {isLogIn, setIsLogIn}= useState(true)
+    const [isLogin, setIsLogin ] = useState(false)
+    console.log(isLogin);
+    const toggleClick= ()=> {
+        setIsLogin(!isLogin)
+    }
     return (
         <Container>
             <div className={s.appBar}>
-                <Header/>
-                {!isLogIn && <LogOutHeaderMenu/>}  
-                {/* <Header />
-            <LogOutHeaderMenu/> */}
+                <Header />
+                <button type='button' onClick={toggleClick}> isLogin</button>
+                {isLogin && <LogOutHeaderMenu/>}  
+              
             </div>
             
        
