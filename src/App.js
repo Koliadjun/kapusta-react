5daa91baba444b91a4e2f8a6f48668e1893b8a9b
Misha Krasnonos <muzishutka7@gmail.com>
2021-12-16 19:13:49 +0200

    ✅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import RegistrationForm from './components/RegistrationForm'
+import RegistrationForm from './components/RegistrationForm';
 import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
 import Modal from 'components/Modal/Modal';
 import ModalContent from 'components/ModalContent/ModalContent';
 import AppBar from 'components/AppBar/AppBar';
 import Summary from 'components/Summary/Summary';
+import IncomeSpendSection from 'components/IncomeSpendSection/IncomeSpendSection';
 
 function App() {
   const [modalActive, setModalActive] = useState(false);
@@ -14,6 +15,7 @@ function App() {
       <RegistrationForm />
       <AppBar />
       <ButtonsBlock />
+      <IncomeSpendSection />
       <Modal active={modalActive} setActive={setModalActive}>
         <ModalContent
           message={'Вы уверены?'}
