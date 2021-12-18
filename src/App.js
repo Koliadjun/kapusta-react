4512f2e057646b6a6d3f34fd061527081f7494f8
ruba-novich <65170516+ruba-novich@users.noreply.github.com>
2021-12-18 12:23:10 +0200

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { useState } from 'react';
-import RegistrationForm from './components/RegistrationForm';
-import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
-import Modal from 'components/Modal/Modal';
-import ModalContent from 'components/ModalContent/ModalContent';
-import AppBar from 'components/AppBar/AppBar';
-import Summary from 'components/Summary/Summary';
-import BalanceModal from './components/InitialBalanceFormModal/Modal/BalanceModal';
-import Content from 'components/InitialBalanceFormModal/Content/Content';
-import Input from 'components/InitialBalanceFormModal/Input/Input';
-import Wrapper from 'components/InitialBalanceFormModal/Wrapper/Wrapper';
-import BalanceForm from 'BalanceForm/BalanceForm';
-import IncomeSpendSection from 'components/IncomeSpendSection/IncomeSpendSection';
+// import React, { useState } from 'react';
+import HomePage from './view/HomePage';
+// import RegistrationForm from './components/RegistrationForm';
+// import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
+// import Modal from 'components/Modal/Modal';
+// import ModalContent from 'components/ModalContent/ModalContent';
+// import AppBar from 'components/AppBar/AppBar';
+// import Summary from 'components/Summary/Summary';
+// import BalanceModal from './components/InitialBalanceFormModal/Modal/BalanceModal';
+// import Content from 'components/InitialBalanceFormModal/Content/Content';
+// import Input from 'components/InitialBalanceFormModal/Input/Input';
+// import Wrapper from 'components/InitialBalanceFormModal/Wrapper/Wrapper';
+// import BalanceForm from 'BalanceForm/BalanceForm';
+// import IncomeSpendSection from 'components/IncomeSpendSection/IncomeSpendSection';
 
 
 function App() {
-  const [modal, setModal] = useState(true);
-  const [modalActive, setModalActive] = useState(false);
-  const sendBalance = () => {
-    setModal(false);
-  };
-  const balance = 55000.55;
+  // const [modal, setModal] = useState(true);
+  // const [modalActive, setModalActive] = useState(false);
+  // const sendBalance = () => {
+  //   setModal(false);
+  // };
+  // const balance = 55000.55;
 
   return (
     <div>
-      <RegistrationForm />
-      <AppBar />
-      <ButtonsBlock />
+      <HomePage></HomePage>
+      {/* <RegistrationForm /> */}
+      {/* <AppBar /> */}
+      {/* <ButtonsBlock />
       <IncomeSpendSection />
       <Modal active={modalActive} setActive={setModalActive}>
         <ModalContent
@@ -43,7 +45,7 @@ function App() {
           <Content />
         </Wrapper>
       </BalanceModal>
-      {!modal === true && <BalanceForm balance={balance} />}
+      {!modal === true && <BalanceForm balance={balance} />} */}
     </div>
   );
 }
