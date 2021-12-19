9ebfebe1ce6fb639d3a7bb6e8603c8a01596a5bd
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-12-19 23:16:15 +0200

    mobile styles

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -9,7 +9,7 @@ export default function CategoryList({ categoryType = 'Категория тов
   const handlerChange = (selectedOption) => {
     setSelectedOption({
       category:categoryType,
-      name:selectedOption.value
+      categoryItem:selectedOption.value
     });
   };
   const filtredArray = Array.from(
@@ -25,13 +25,68 @@ export default function CategoryList({ categoryType = 'Категория тов
   );
   const options=filtredArray.filter(item=>item!==undefined)
 
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: 'none',
+    color:state.isFocused? 'var(--main-text-color)':'var(--placeholders-text-color)',
+    padding:'10px 20px',
+    backgroundColor:state.isFocused? 'var(--secondary-background-color)': 'transparent'   
+  }),
+  placeholder:()=>({
+    color:'var(--placeholders-text-color)',
+    paddingLeft:'20px'
+  }),
+  control:()=>({
+    backgroundColor:'transparent',
+    borderStyle:'none',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'spaceBetween'
+  }),
+  indicatorSeparator:()=>({
+    width:'0'
+  }),
+  valueContainer:()=>({
+    marginRight:'125px'
+ 
+  }),
+  indicatorsContainer:(provided)=>({
+    ...provided,
+    padding:'0',
+
+  }),
+  indicatorContainer:(provided)=>({
+    ...provided,
+    padding:'0'
+
+  }),
+  input:()=>({
+    display:'none'
+  }),
+  menu:()=>({
+    boxShadow: '0px 10px 60px rgba(170, 178, 197, 0.2);'
+  }),
+  menuList:()=>({
+    overflowY:''
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = 'opacity 300ms';  
+    return { ...provided, opacity, transition,color:'var(--placeholders-text-color)',paddingLeft:'20px' };
+  }
+}
+
+
   return (
-    <form>
+    <div className={s.categoryForm}>
       <Select
+      styles={customStyles}
        placeholder={categoryType}
         onChange={handlerChange}
         options={options}
+        className={s.categoryFormSelect}
       />
-    </form>
+    </div>
   );
 }
