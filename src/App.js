f026b27400572d285c8c8bab50a36de6c28ed768
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-22 01:29:05 +0200

    comment not used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Routes,
   Route,
-  Link,
-  Outlet,
+  // Link,
+  // Outlet,
   Navigate,
   useNavigate,
 } from 'react-router-dom';
-import RegistrationForm from './components/RegistrationForm';
-import CategoryList from 'components/CategoryList';
-import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
-import Modal from 'components/Modal/Modal';
-import ModalContent from 'components/ModalContent/ModalContent';
-import AppBar from 'components/AppBar/AppBar';
-import Summary from 'components/Summary/Summary';
-import BalanceModal from './components/InitialBalanceFormModal/Modal/BalanceModal';
-import Content from 'components/InitialBalanceFormModal/Content/Content';
-import Input from 'components/InitialBalanceFormModal/Input/Input';
-import Wrapper from 'components/InitialBalanceFormModal/Wrapper/Wrapper';
-import BalanceForm from 'BalanceForm/BalanceForm';
-import IncomeSpendSection from 'components/IncomeSpendSection/IncomeSpendSection';
+// import RegistrationForm from './components/RegistrationForm';
+// import CategoryList from 'components/CategoryList';
+// import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
+// import Modal from 'components/Modal/Modal';
+// import ModalContent from 'components/ModalContent/ModalContent';
+// import AppBar from 'components/AppBar/AppBar';
+// import Summary from 'components/Summary/Summary';
+// import BalanceModal from './components/InitialBalanceFormModal/Modal/BalanceModal';
+// import Content from 'components/InitialBalanceFormModal/Content/Content';
+// import Input from 'components/InitialBalanceFormModal/Input/Input';
+// import Wrapper from 'components/InitialBalanceFormModal/Wrapper/Wrapper';
+// import BalanceForm from 'BalanceForm/BalanceForm';
+// import IncomeSpendSection from 'components/IncomeSpendSection/IncomeSpendSection';
 import Loader from 'components/Loader';
 
 import CommentView from './views/CommentView';
@@ -28,11 +28,11 @@ import ReportView from './views/ReportView';
 import { authOperations, authSelectors } from 'redux/auth';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import HomePage from './views/HomePage';
+// import HomePage from './views/HomePage';
 
 // import './App.css';
 // import Transactionslist from 'components/Transactionslist/Transactionslist';
-import Container from 'components/Container/Container';
+// import Container from 'components/Container/Container';
 
 // import InputBalance from 'components/InputBalance/InputBalance';
 // import InputRegister from 'components/InputRegister/InputRegister';
@@ -40,17 +40,17 @@ import Container from 'components/Container/Container';
 // import HomePage from './view/HomePage';
 // import BalanceModal from './components/InitialBalanceFormModal/Modal/BalanceModal';
 // import Content from 'components/InitialBalanceFormModal/Content/Content';
-import CategoryImagesList from 'components/CategoryImages/CategoryImagesList/CategoryImagesList';
+// import CategoryImagesList from 'components/CategoryImages/CategoryImagesList/CategoryImagesList';
 // import Tabs from 'components/Tabs/Tabs';
 
 function App() {
-  const [modal, setModal] = useState(true);
-  const [modalActive, setModalActive] = useState(false);
-  const sendBalance = () => {
-    setModal(false);
-  };
+  // const [modal, setModal] = useState(true);
+  // const [modalActive, setModalActive] = useState(false);
+  // const sendBalance = () => {
+  //   setModal(false);
+  // };
 
-  const [balance, setBalance] = useState(0);
+  // const [balance, setBalance] = useState(0);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -63,14 +63,14 @@ function App() {
     if (isGoogled) {
       navigate('/report');
     }
-  }, [dispatch, isGoogled]);
+  }, [dispatch, isGoogled, navigate]);
 
   return isFetchingUser ? (
     <Loader />
   ) : (
     <div>
-      <ButtonsBlock />
-      <Modal active={modalActive} setActive={setModalActive} />
+      {/* <ButtonsBlock />
+      <Modal active={modalActive} setActive={setModalActive} /> */}
       <Routes>
         <Route exact path="/" element={<Navigate to="home" />} />
         <Route
@@ -91,7 +91,7 @@ function App() {
         />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      <AppBar />
+      {/* <AppBar />
       <ButtonsBlock />
       <IncomeSpendSection />
       <Modal active={modalActive} setActive={setModalActive}>
@@ -127,7 +127,7 @@ function App() {
         </Wrapper>
       </BalanceModal>
       {!modal === true && <BalanceForm balance={balance} />}
-      <Loader />
+      <Loader /> */}
     </div>
   );
 }
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,28 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import RegistrationForm from '../components/RegistrationForm';
 import queryString from 'query-string';
 import { authOperations } from '../redux/auth';
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  useDispatch,
+  // useSelector
+} from 'react-redux';
 
-import { authSelectors } from '../redux/auth';
-import axios from 'axios';
+// import { authSelectors } from '../redux/auth';
+// import axios from 'axios';
 
-function HomeView({...props}) {
+function HomeView({ ...props }) {
   const dispatch = useDispatch();
-  const [firstLoaded, setFirstLoaded] = useState(true)
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn)
+  const [firstLoaded, setFirstLoaded] = useState(true);
+  // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn)
   const token = Object.values(queryString.parse(window.location.href))[0];
-  console.log(props)
-  useEffect(()=> {
+  console.log(props);
+  useEffect(() => {
     if (firstLoaded && token) {
-        setFirstLoaded(false)
-        dispatch(authOperations.isGooglingUser(token))
-        setFirstLoaded(false)
-      }
-  }, [])
-
-  
-  
+      setFirstLoaded(false);
+      dispatch(authOperations.isGooglingUser(token));
+      setFirstLoaded(false);
+    }
+  }, [dispatch, firstLoaded, token]);
 
   return (
     <div>
