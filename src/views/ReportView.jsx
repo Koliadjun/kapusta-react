6ae5cf8b3298097bb9d2a0c6e8c7eb317f4492d6
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-12-22 21:23:38 +0200

    убрала appBar из вьюхи

--- src/views/ReportView.jsx
+++ src/views/ReportView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import {Outlet} from 'react-router-dom'
 import Container from '../components/Container/Container';
-import AppBar from '../components/AppBar/AppBar';
+// import AppBar from '../components/AppBar/AppBar';
 // import BalanceLine from '../components/BalanceLine/BalanceLine';
 import IncomeSpendSection from '../components/IncomeSpendSection/IncomeSpendSection';
 function ReportView({ children }) {
@@ -10,7 +10,7 @@ function ReportView({ children }) {
       {children}
       <h1>ReportView</h1>
       <Container>
-        <AppBar />
+        {/* <AppBar /> */}
         {/* <BalanceLine /> */}
         <IncomeSpendSection />
       </Container>
