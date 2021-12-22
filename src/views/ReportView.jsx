402e3a6ade6dab2a3bfaf42daf294c94ecd166b8
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-22 21:02:38 +0200

    add report

--- src/views/ReportView.jsx
+++ src/views/ReportView.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 // import {Outlet} from 'react-router-dom'
-
-function ReportView({children}) {
-    return (
-        <div>
-            {children}
-            <h1>ReportView</h1>
-        </div>
-    )
+import Container from '../components/Container/Container';
+import AppBar from '../components/AppBar/AppBar';
+// import BalanceLine from '../components/BalanceLine/BalanceLine';
+import IncomeSpendSection from '../components/IncomeSpendSection/IncomeSpendSection';
+function ReportView({ children }) {
+  return (
+    <>
+      {children}
+      <h1>ReportView</h1>
+      <Container>
+        <AppBar />
+        {/* <BalanceLine /> */}
+        <IncomeSpendSection />
+      </Container>
+    </>
+  );
 }
 
-export default ReportView
+export default ReportView;
