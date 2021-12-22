1666a1fab4382b2122b39ff3cfb5759d756ce0d2
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-22 12:11:53 +0200

    make preatty app.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,7 @@ import {
   Navigate,
   useNavigate,
 } from 'react-router-dom';
-// import RegistrationForm from './components/RegistrationForm';
-// import CategoryList from 'components/CategoryList';
-// import ButtonsBlock from 'components/ButtonsBlock/ButtonsBlock';
-// import Modal from 'components/Modal/Modal';
-// import ModalContent from 'components/ModalContent/ModalContent';
-// import AppBar from 'components/AppBar/AppBar';
-// import Summary from 'components/Summary/Summary';
-// import BalanceModal from './components/InitialBalanceFormModal/Modal/BalanceModal';
-// import Content from 'components/InitialBalanceFormModal/Content/Content';
-// import Input from 'components/InitialBalanceFormModal/Input/Input';
-// import Wrapper from 'components/InitialBalanceFormModal/Wrapper/Wrapper';
-// import BalanceForm from 'BalanceForm/BalanceForm';
-// import IncomeSpendSection from 'components/IncomeSpendSection/IncomeSpendSection';
+
 import Loader from 'components/Loader';
 
 import CommentView from './views/CommentView';
@@ -28,33 +16,11 @@ import ReportView from './views/ReportView';
 import { authOperations, authSelectors } from 'redux/auth';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import HomePage from './views/HomePage';
-
-// import './App.css';
-// import Transactionslist from 'components/Transactionslist/Transactionslist';
-// import Container from 'components/Container/Container';
-
-// import InputBalance from 'components/InputBalance/InputBalance';
-// import InputRegister from 'components/InputRegister/InputRegister';
-// import InputDescriptionProduct from 'components/InputDescriptionProduct/InputDescriptionProduct';
-// import HomePage from './view/HomePage';
-// import BalanceModal from './components/InitialBalanceFormModal/Modal/BalanceModal';
-// import Content from 'components/InitialBalanceFormModal/Content/Content';
-// import CategoryImagesList from 'components/CategoryImages/CategoryImagesList/CategoryImagesList';
-// import Tabs from 'components/Tabs/Tabs';
-
 function App() {
-  // const [modal, setModal] = useState(true);
-  // const [modalActive, setModalActive] = useState(false);
-  // const sendBalance = () => {
-  //   setModal(false);
-  // };
-
-  // const [balance, setBalance] = useState(0);
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const isFetchingUser = useSelector(authSelectors.getIsFetchingUser);
+  // const isLoggedin = true
   const isLoggedin = useSelector(authSelectors.getIsLoggedIn);
   const isGoogled = useSelector(authSelectors.getIsGoogled);
 
@@ -68,9 +34,7 @@ function App() {
   return isFetchingUser ? (
     <Loader />
   ) : (
-    <div>
-      {/* <ButtonsBlock />
-      <Modal active={modalActive} setActive={setModalActive} /> */}
+    <>
       <Routes>
         <Route exact path="/" element={<Navigate to="home" />} />
         <Route
@@ -91,44 +55,7 @@ function App() {
         />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      {/* <AppBar />
-      <ButtonsBlock />
-      <IncomeSpendSection />
-      <Modal active={modalActive} setActive={setModalActive}>
-        <ModalContent
-          message={'Вы уверены?'}
-          textLeftButton={'да'}
-          textRightButton={'нет'}
-        />
-      </Modal>
-      <button onClick={() => setModalActive(true)}>Проверка модалки</button>
-      <BalanceModal visible={modal} setVisible={setModal}>
-        <Wrapper>
-          <Input sendBalance={sendBalance} setBalance={setBalance} />
-
-          <button onClick={() => setModalActive(true)}>Проверка модалки</button>
-          <Summary />
-
-          <Content />
-        </Wrapper>
-      </BalanceModal>
-      {!modal === true && <BalanceForm balance={balance} />}
-      <Loader />
-      <CategoryList />
-      <CategoryImagesList />
-
-      <button onClick={() => setModalActive(true)}>Проверка модалки</button>
-      <Summary />
-      <BalanceModal visible={modal} setVisible={setModal}>
-        <Wrapper>
-          <Input sendBalance={sendBalance} setBalance={setBalance} />
-
-          <Content />
-        </Wrapper>
-      </BalanceModal>
-      {!modal === true && <BalanceForm balance={balance} />}
-      <Loader /> */}
-    </div>
+    </>
   );
 }
 
