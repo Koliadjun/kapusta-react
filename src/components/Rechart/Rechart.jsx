dd8ae85344d85f5aa51292c76e36c5c8e23dcac3
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-12-22 13:53:01 +0200

    add some styles

--- src/components/Rechart/Rechart.jsx
+++ src/components/Rechart/Rechart.jsx
@@ -1,40 +1,77 @@
 import React from 'react';
-import { BarChart, Bar, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, LabelList } from 'recharts';
+import Container from 'components/Container/Container';
+
 import s from './styles.module.css';
 
 export default function Rechart() {
   const data = [
     {
-        name: 'Page A',
-        amt: 2400,
-      },
-      {
-        name: 'Page B',
-        amt: 1210,
-      },
-      {
-        name: 'Page C',
-        amt: 9290,
-      },
-      {
-        name: 'Page D',
-        amt: 8790,
-      }
+      name: 'Page A',
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      amt: 1210,
+    },
+    {
+      name: 'Page C',
+      amt: 9290,
+    },
+    {
+      name: 'Page D',
+      amt: 8790,
+    },
+    {
+      name: 'Page D',
+      amt: 790,
+    },
   ];
-  
-  const sortedData=data.sort((a,b)=>b.amt-a.amt);
-  
-  const renderCustomBarLabel = ({ payload, x, y, width, height, name }) => {
-    return <text x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>{`${name}`}</text>;
+
+  const sortedData = data.sort((a, b) => b.amt - a.amt);
+  const renderCustomBarLabel = ({ x, y, width, value }) => {
+    return (
+      <text
+        x={x + width / 2}
+        y={y - 4}
+        fill="#666"
+        textAnchor="middle"
+        dy={-10}
+        
+       
+      >{`${value} грн`}</text>
+    );
   };
+  const renderCategoryLabel = ({ x, y, value }) => (
+    <text x={x} y={y} dy={-10} fontSize={10} fontColor='var(--main-text-color)'>
+        {value}
+    </text>
+);
+const barSize=()=>{
+  if(window.screen.width<768){
+    return 15
+  }else if(window.screen.width>=768){
+    return 38
+  }return;
+}
 
   return (
-   <ResponsiveContainer width='100%' height={282}>
-        <BarChart width={485} height={282} data={sortedData}>
-        <Bar dataKey="amt" fill="#8884d8" barSize={15} label={renderCustomBarLabel} radius={[10,10,0, 0]}/>
+    <div className={s.rechartSection}>
+      <BarChart width={282} height={400} data={sortedData} barGap="15" margin={{ top: 50, right: 15, bottom: 9, left: 15 }} >
+        <XAxis dataKey="name" axisLine={false} tickLine={false} dy={5}></XAxis>
+        <Bar
+          dataKey="amt"
+          barSize={barSize}
+          radius={[10, 10, 0, 0]}
+          label={renderCustomBarLabel}
+        >
+          <LabelList
+            dataKey="description"
+            content={renderCategoryLabel}
+            fill="#52555F"
+          />
+        </Bar>
       </BarChart>
-   </ResponsiveContainer>
-     
-   
+    </div>
   );
 }
