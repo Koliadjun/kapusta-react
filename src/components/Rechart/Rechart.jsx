35af2e888a7ced7c9ca6c693e7900f6182e5f942
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-12-23 16:06:43 +0200

    tablet,desktop styles

--- src/components/Rechart/Rechart.jsx
+++ src/components/Rechart/Rechart.jsx
@@ -1,74 +1,102 @@
 import React, { useState } from 'react';
-import { BarChart, Bar, XAxis, YAxis, LabelList } from 'recharts';
-import Container from 'components/Container/Container';
-
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  LabelList,
+  CartesianGrid,
+} from 'recharts';
+import PropTypes from 'prop-types';
 import s from './styles.module.css';
 
-export default function Rechart() {
-  // const [data,setData]=useState([])
-  const data = [
-    {
-      name: 'Page A',
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      amt: 7210,
-    },
-    {
-      name: 'Page C',
-      amt: 9290,
-    },
-    {
-      name: 'Page D',
-      amt: 8790,
-    },
-    {
-      name: 'Page D',
-      amt: 6790,
-    },
-  ];
+export default function Rechart({ itemsArray }) {
+  const sortedData = itemsArray.sort((a, b) => b.value - a.value);
 
-  const sortedData = data.sort((a, b) => b.amt - a.amt);
   const renderCustomBarLabel = ({ x, y, width, value }) => {
+    if (window.screen.width < 768) {
+      return (
+        <text
+          x={width + x}
+          y={y - 4}
+          fill="#666"
+          textAnchor="middle"
+          dy={-10}
+          fontSize={10}
+          fontColor="var(--main-text-color)"
+        >{`${value} грн`}</text>
+      );
+    }
     return (
       <text
-        x={width+x}
-        y={y - 4}
+        x={x + width / 2}
+        y={y}
         fill="#666"
         textAnchor="middle"
         dy={-10}
-        fontSize={10} fontColor="var(--main-text-color)"
+        fontSize={10}
+        fontColor="var(--main-text-color)"
       >{`${value} грн`}</text>
     );
   };
-  const renderCategoryLabel = ({ x, y, name }) => (
-    <text x={x} y={y-4} dy={-10} fontSize={10} fontColor="var(--main-text-color)" textAnchor="start">
-      {name}
-    </text>
-  );
-  const barSize = () => {
+  const renderCategoryLabel = ({ x, y, name, width, height }) => {
     if (window.screen.width < 768) {
-      return 15;
+      return (
+        <text
+          x={x}
+          y={y - 4}
+          dy={-10}
+          fontSize={10}
+          fontColor="var(--main-text-color)"
+          textAnchor="start"
+        >
+          {name}
+        </text>
+      );
     }
-    return;
+    return (
+      <text
+        x={x}
+        y={y + height + 20}
+        dy={-10}
+        fontSize={10}
+        fontColor="var(--main-text-color)"
+        textAnchor="start"
+      >
+        {name}
+      </text>
+    );
   };
 
   return (
     <div className={s.rechartSection}>
       <BarChart
-        width={282}
-        height={400}
+        width={window.screen.width < 768 ? 282 : 422}
+        height={window.screen.width < 768 ? 400 : 580}
         data={sortedData}
-        margin={{ top: 50, right: 15, bottom: 9, left: 15 }}
-        layout="vertical"
+        margin={{ top: 50, right: 15, bottom: 15, left: 15 }}
+        layout={window.screen.width < 768 ? 'vertical' : 'horizontal'}
       >
-        <XAxis hide axisLine={false} type="number" />
-        <YAxis dataKey="description" type="category" hide />
+        {window.screen.width < 768 ? (
+          <>
+            <XAxis hide axisLine={false} type="number" />
+            <YAxis dataKey="description" type="category" hide />
+          </>
+        ) : (
+          <>
+            <YAxis hide axisLine={false} type="number" />
+            <XAxis dataKey="description" type="category" hide />
+            <CartesianGrid
+              vertical={false}
+              color="var(--secondary-background-color)"
+            />
+          </>
+        )}
+
         <Bar
-          dataKey="amt"
-          barSize={15}
-          radius={[0, 10, 10, 0]}
+          dataKey="value"
+          barSize={window.screen.width < 768 ? 15 : 38}
+          radius={window.screen.width < 768 ? [0, 10, 10, 0] : [10, 10, 0, 0]}
           label={renderCustomBarLabel}
         >
           <LabelList
@@ -81,3 +109,7 @@ export default function Rechart() {
     </div>
   );
 }
+
+Rechart.propTypes = {
+  itemsArray: PropTypes.array
+};
