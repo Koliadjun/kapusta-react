419278966a7f7d4b8c70316e226a7b0bb6902019
Anzhela Boiko <76243473+AngieBoiko@users.noreply.github.com>
2021-12-23 13:02:17 +0200

    mobile view

--- src/components/Rechart/Rechart.jsx
+++ src/components/Rechart/Rechart.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BarChart, Bar, XAxis, LabelList } from 'recharts';
+import React, { useState } from 'react';
+import { BarChart, Bar, XAxis, YAxis, LabelList } from 'recharts';
 import Container from 'components/Container/Container';
 
 import s from './styles.module.css';
 
 export default function Rechart() {
+  // const [data,setData]=useState([])
   const data = [
     {
       name: 'Page A',
@@ -12,7 +13,7 @@ export default function Rechart() {
     },
     {
       name: 'Page B',
-      amt: 1210,
+      amt: 7210,
     },
     {
       name: 'Page C',
@@ -24,7 +25,7 @@ export default function Rechart() {
     },
     {
       name: 'Page D',
-      amt: 790,
+      amt: 6790,
     },
   ];
 
@@ -32,37 +33,42 @@ export default function Rechart() {
   const renderCustomBarLabel = ({ x, y, width, value }) => {
     return (
       <text
-        x={x + width / 2}
+        x={width+x}
         y={y - 4}
         fill="#666"
         textAnchor="middle"
         dy={-10}
-        
-       
+        fontSize={10} fontColor="var(--main-text-color)"
       >{`${value} грн`}</text>
     );
   };
-  const renderCategoryLabel = ({ x, y, value }) => (
-    <text x={x} y={y} dy={-10} fontSize={10} fontColor='var(--main-text-color)'>
-        {value}
+  const renderCategoryLabel = ({ x, y, name }) => (
+    <text x={x} y={y-4} dy={-10} fontSize={10} fontColor="var(--main-text-color)" textAnchor="start">
+      {name}
     </text>
-);
-const barSize=()=>{
-  if(window.screen.width<768){
-    return 15
-  }else if(window.screen.width>=768){
-    return 38
-  }return;
-}
+  );
+  const barSize = () => {
+    if (window.screen.width < 768) {
+      return 15;
+    }
+    return;
+  };
 
   return (
     <div className={s.rechartSection}>
-      <BarChart width={282} height={400} data={sortedData} barGap="15" margin={{ top: 50, right: 15, bottom: 9, left: 15 }} >
-        <XAxis dataKey="name" axisLine={false} tickLine={false} dy={5}></XAxis>
+      <BarChart
+        width={282}
+        height={400}
+        data={sortedData}
+        margin={{ top: 50, right: 15, bottom: 9, left: 15 }}
+        layout="vertical"
+      >
+        <XAxis hide axisLine={false} type="number" />
+        <YAxis dataKey="description" type="category" hide />
         <Bar
           dataKey="amt"
-          barSize={barSize}
-          radius={[10, 10, 0, 0]}
+          barSize={15}
+          radius={[0, 10, 10, 0]}
           label={renderCustomBarLabel}
         >
           <LabelList
