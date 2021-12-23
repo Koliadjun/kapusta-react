b0d16e7baddfb3fa21e8145f50336897f044115e
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-23 14:35:52 +0200

    fix

--- src/components/LogOutHeaderMenu/LogOutHeaderMenu.jsx
+++ src/components/LogOutHeaderMenu/LogOutHeaderMenu.jsx
@@ -1,89 +1,85 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { authOperations, authSelectors } from '../../redux/auth';
-import sprite from '../../images/svg/sprite.svg'
-import useWindowDimensions from '../../hooks/useWindowDimensions'
-import users from './users.json'
+import sprite from '../../images/svg/sprite.svg';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+import users from './users.json';
 import Avatar from 'react-avatar';
 import shortid from 'shortid';
-import s from './LogOutHeaderMenu.module.css'
-import Modal from '../Modal/Modal'
-import ModalContent from '../ModalContent/ModalContent'
-import ButtonsBlock from '../ButtonsBlock/ButtonsBlock'
+import s from './LogOutHeaderMenu.module.css';
+import Modal from '../Modal/Modal';
+import ModalContent from '../ModalContent/ModalContent';
+// import ButtonsBlock from '../ButtonsBlock/ButtonsBlock'
 
 function LogInHeaderMenu() {
-  const name = useSelector(authSelectors.getName)
+  const name = useSelector(authSelectors.getName);
   const [modal, setModal] = useState(false);
   // const {modalActive, setModalActive}=useState(false)
   const userId = shortid.generate();
   const viewPort = useWindowDimensions();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const toggleBtn = () => {
-    setModal(prevModal => !prevModal)
-  }
+    setModal(prevModal => !prevModal);
+  };
   const logOutModal = () => {
     dispatch(authOperations.logOut());
-    toggleBtn()
-  }
- 
+    toggleBtn();
+  };
+
   return (
-        <div className={s.userMenu}>
+    <div className={s.userMenu}>
       {viewPort.width >= 768 && (
-       <>
-            <Avatar
-              className={s.userAvatar}
-             size="32"
-             color={Avatar.getRandomColor('sitebase', [
-                'red',
-                'green',
-                'blue',
-                'orange',
-              ])}
-      />
-           <div className={s.line}>
-        {users.map(user => (
-          <p className={s.userName} key={userId}>{name}</p>))}
-     </div> 
-      <button className={s.logoutBtn} onClick={toggleBtn}>
-        <p className={s.textBtn}>Выйти</p>
-      </button>
-       
+        <>
+          <Avatar
+            className={s.userAvatar}
+            size="32"
+            color={Avatar.getRandomColor('sitebase', [
+              'red',
+              'green',
+              'blue',
+              'orange',
+            ])}
+          />
+          <div className={s.line}>
+            {users.map(user => (
+              <p className={s.userName} key={userId}>
+                {name}
+              </p>
+            ))}
+          </div>
+          <button className={s.logoutBtn} onClick={toggleBtn}>
+            <p className={s.textBtn}>Выйти</p>
+          </button>
         </>
       )}
-      {viewPort.width < 768 &&(
+      {viewPort.width < 768 && (
         <>
           <div className={s.userMenu}>
-          <Avatar
-        Avatar
+            <Avatar
+              Avatar
               className={s.userAvatar}
-        size="25"
-        
+              size="25"
               color={Avatar.getRandomColor('sitebase', [
                 'red',
                 'green',
                 'blue',
                 'orange',
               ])}
-          />
-          <button className={s.iconButton} onClick={toggleBtn}>
-              <svg 
-              className={s.logOutIcon}
-              >
-            <use xlinkHref={`${sprite}#icon-logout`}/>
-        </svg>
-          </button>
-         </div>
-      </>
+            />
+            <button className={s.iconButton} onClick={toggleBtn}>
+              <svg className={s.logOutIcon}>
+                <use xlinkHref={`${sprite}#icon-logout`} />
+              </svg>
+            </button>
+          </div>
+        </>
       )}
-      {modal &&
-        <div className={s.hederModalOut} >
-           
-          <Modal active={modal}
-              setActive={setModal}
-             >
-          <ModalContent
+      {modal && (
+        <div className={s.hederModalOut}>
+          <Modal active={modal} setActive={setModal}>
+            <ModalContent
               message={'Вы уверены?'}
               textLeftButton={'да'}
               textRightButton={'нет'}
@@ -91,10 +87,10 @@ function LogInHeaderMenu() {
               onClickLeftButton={logOutModal}
             />
           </Modal>
-     </div> 
-      }
-     </div>   
-    )
- }
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default LogInHeaderMenu;
