1c39dcfdb94be9fc8225ea0aea10a7d58bc68fab
funnykoss <75586141+funnykoss@users.noreply.github.com>
2021-12-23 08:50:36 +0200

    logout

--- src/components/LogOutHeaderMenu/LogOutHeaderMenu.jsx
+++ src/components/LogOutHeaderMenu/LogOutHeaderMenu.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import operations from '../../redux/auth/auth-operations'
 import sprite from '../../images/svg/sprite.svg'
 import useWindowDimensions from '../../hooks/useWindowDimensions'
 import users from './users.json'
@@ -9,16 +11,26 @@ import s from './LogOutHeaderMenu.module.css'
 import Modal from '../Modal/Modal'
 import ModalContent from '../ModalContent/ModalContent'
 
-function LogInHeaderMenu() {
+
+function LogInHeaderMenu({onClickLeftButton, onClickRightButton, onClick},) {
+  // const post=  
   const [modal, setModal] = useState(false);
   const {isModalOpen, setIsModalOpen}=useState(false)
   const userId = shortid.generate();
   const viewPort = useWindowDimensions();
+  const dispatch = useDispatch()
+  const logOut = useSelector(operations.logOut)
 
   const toggleBtn = () => {
-    console.log(modal);
   setModal(prevModal=>!prevModal)
-}
+  }
+
+  console.log(logOut);
+
+  const logOutModal = () => {
+    dispatch(logOut);
+
+  }
 
   return (
         <div className={s.userMenu}>
@@ -74,10 +86,18 @@ function LogInHeaderMenu() {
       )}
       {modal &&
         <div className={s.hederModalOut }>
-          <Modal active={isModalOpen} setActive={setIsModalOpen} /><ModalContent
+          <Modal
+            active={isModalOpen}
+            setActive={setIsModalOpen}
+            onClickLeftButton={logOutModal}
+            onClickRightButton={toggleBtn}
+            onClose={toggleBtn }
+          />
+          <ModalContent   
           message={'Вы уверены?'}
           textLeftButton={'да'}
-          textRightButton={'нет'} />
+            textRightButton={'нет'} />
+            
       </div>
       }
      </div>   
