e10ac25ba1ae0e13d0f807ab307329cb170f2ec9
Koliadzhyn Yurii <Koliadjun@gmail.com>
2021-12-23 18:06:48 +0200

    fix

--- src/components/CategoryImages/CategoryImagesList/CategoryImagesList.jsx
+++ src/components/CategoryImages/CategoryImagesList/CategoryImagesList.jsx
@@ -1,54 +1,69 @@
-import React, {useState} from 'react';
+import Rechart from 'components/Rechart/Rechart';
+import React, { useState } from 'react';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
-import CategoryCoast from '../CategoryData/CategoryCoast'
+import { useSelector } from 'react-redux';
+import { transactionSelectors } from 'redux/transaction';
+import CategoryCoast from '../CategoryData/CategoryCoast';
 import CategoryIncome from '../CategoryData/CategoryIncome';
-import s from './CategoryImagesList.module.css'
-
+import s from './CategoryImagesList.module.css';
 
 const CategoryImagesList = () => {
-      const [typeTrans, setTypeTrans] = useState('expenses');
+  const [typeTrans, setTypeTrans] = useState('expenses');
 
-    const handleClick = ()=>{
-        if (typeTrans === 'incoming') {
-          setTypeTrans('expenses')
-        }
-        if (typeTrans === 'expenses') {
-            setTypeTrans('incoming')
-        }
-  }
-    
-    return (
+  const handleClick = () => {
+    if (typeTrans === 'incoming') {
+      setTypeTrans('expenses');
+    }
+    if (typeTrans === 'expenses') {
+      setTypeTrans('incoming');
+    }
+  };
+  const year = useSelector(transactionSelectors.getCurrentYear);
+  const month = useSelector(transactionSelectors.getCurrentMonth);
+  const coast = useSelector(
+    transactionSelectors.getSpendReportDataPerMonth(month, year),
+  );
+  const income = useSelector(
+    transactionSelectors.getIncomeReportDataPerMonth(month, year),
+  );
+  return (
     <div className={s.listWrapper}>
-    <div className={s.wrapper}>
+      <div className={s.wrapper}>
         <button
-                className={s.btnLeft}
-                type="button"
-                onClick={() => handleClick()}
-            >
-            <MdKeyboardArrowLeft size={20} style={{ color: '#FF751D' }} />
+          className={s.btnLeft}
+          type="button"
+          onClick={() => handleClick()}
+        >
+          <MdKeyboardArrowLeft size={20} style={{ color: '#FF751D' }} />
         </button>
-       {typeTrans === 'expenses' ? (
+        {typeTrans === 'expenses' ? (
           <p className={s.title}> Расходы </p>
         ) : (
           <p className={s.title}> Доходы </p>
-                )}
+        )}
         <button
-                className={s.btnRight}
-                type="button"
-                onClick={() => handleClick()}
-            >
-            <MdKeyboardArrowRight size={20} style={{ color: '#FF751D' }} />
-        </button>        
-            </div>
-            <div>
-                {typeTrans === 'expenses' ? (
-                    <CategoryCoast />
-                ) : (
-                    < CategoryIncome/>   
-                )}
-            </div>
-        </div>
-    )
-}
+          className={s.btnRight}
+          type="button"
+          onClick={() => handleClick()}
+        >
+          <MdKeyboardArrowRight size={20} style={{ color: '#FF751D' }} />
+        </button>
+      </div>
+      <div>
+        {typeTrans === 'expenses' ? (
+          <>
+            <CategoryCoast />
+            <Rechart itemsArray={coast} />
+          </>
+        ) : (
+          <>
+            <CategoryIncome />
+            <Rechart itemsArray={income} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default CategoryImagesList
+export default CategoryImagesList;
--- src/components/LogoHeader/LogoHeader.jsx
+++ src/components/LogoHeader/LogoHeader.jsx
@@ -1,5 +1,10 @@
-import s from './LogoHeader.module.css'
+import { Link } from 'react-router-dom';
+import s from './LogoHeader.module.css';
 
 export default function logoHeader() {
-      return <span className={s.link} aria-label="Логотип сайта"></span>;
+  return (
+    <Link to="/">
+      <span className={s.link} aria-label="Логотип сайта"></span>
+    </Link>
+  );
 }
